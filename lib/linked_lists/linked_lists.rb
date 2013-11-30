7677043a79c05cbaa51838ca4205c31b651aab10
Natasha Hull <musicalnat@gmail.com>
2013-11-29 22:59:40 -0800

    Started testing and debugging

--- lib/linked_lists/linked_lists.rb
+++ lib/linked_lists/linked_lists.rb
@@ -9,21 +9,27 @@ class LinkedList
 
   def delete(item)
     if item == head
-      head = head.next_item
+      self.head = head.next_item
     else
       head.delete(item)
     end
   end
 
   def insert_after(f_item, new_item)
-    if(f_item.nil?)
+    if f_item.nil?
       n_item = LinkedListItem.new(new_item, head)
-      head = n_item
+      self.head = n_item
     else
       first_item = find(f_item)
       first_item.insert_after(new_item) if first_item
     end
   end
+
+  private
+
+    def head=(val)
+      @head = val
+    end
 end
 
 class LinkedListItem
@@ -44,14 +50,14 @@ class LinkedListItem
   def delete(val)
     return unless next_item
     if next_item == val
-      next_item = next_item.next_item
+      self.next_item = next_item.next_item
     else
       next_item.delete(val)
     end
   end
 
   def insert_after(val)
-    next_item = LinkedListItem.new(val, next_item)
+    self.next_item = LinkedListItem.new(val, next_item)
   end
 
   def ==(val)
---
+++ spec/linked_lists/linked_list_spec.rb
@@ -0,0 +1,54 @@
+require 'rspec'
+require 'linked_lists/linked_lists.rb'
+
+describe LinkedList do
+  context "sanity check" do
+    it "creates a list" do
+      LinkedList.new
+    end
+
+    it "adds a head element to the list" do
+      list = LinkedList.new
+      list.insert_after(nil, 1)
+      list.head.should be_a(LinkedListItem)
+      list.head.value.should == 1
+    end
+
+    it "finds elements in the list" do
+      list = LinkedList.new
+      list.insert_after(nil, 1)
+      list.find(1).value.should == 1
+    end
+
+    it "adds more elements to the list" do
+      list = LinkedList.new
+      list.insert_after(nil, 1)
+      list.insert_after(1, 2)
+      list.insert_after(1, 3)
+      list.insert_after(2, 4)
+      list.find(4).value.should == 4
+    end
+
+    it "deletes elements in the list" do
+      list = LinkedList.new
+      list.insert_after(nil, 1)
+      list.insert_after(1, 2)
+      list.insert_after(1, 3)
+      list.insert_after(2, 4)
+      list.delete(4)
+
+      list.find(4).should be_nil
+    end
+
+    it "successfully removes the head from the list" do
+      list = LinkedList.new
+      list.insert_after(nil, 1)
+      list.insert_after(1, 2)
+      list.insert_after(1, 3)
+      list.insert_after(2, 4)
+      list.delete(1)
+
+      list.head.value.should == 3
+    end
+  end
+end
