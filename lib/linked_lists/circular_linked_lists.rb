3d0f24d01e4baddecedbed8bced8860d81c72b4d
Natasha Hull <musicalnat@gmail.com>
2013-11-30 16:27:44 -0800

    Added tests for a fixed a few things in circular linked lists

--- lib/linked_lists/circular_linked_lists.rb
+++ lib/linked_lists/circular_linked_lists.rb
@@ -5,7 +5,20 @@ class CircularLinkedList < LinkedList
   end
 
   def delete(item)
-    head.delete(item, head)
+    if item == head
+      self.head = head.next_item
+    else
+      head.delete(item, head)
+    end
+  end
+
+  def insert_after(f_item, new_item)
+    if f_item.nil?
+      n_item = CircularLinkedListItem.new(new_item, head)
+      self.head = n_item
+    else
+      super(f_item, new_item)
+    end
   end
 end
 
@@ -16,12 +29,24 @@ class CircularLinkedListItem < LinkedListItem
   end
 
   def find(val, stop)
-    return if next_item == stop
-    super(val)
+    return if (next_item == stop) && (value != val)
+    if value == val
+      self
+    else
+      next_item.find(val, stop) if !!next_item
+    end
   end
 
   def delete(val, stop)
     return if next_item == stop
-    super(val)
+    if next_item == val
+      self.next_item = next_item.next_item
+    else
+      next_item.delete(val, stop)
+    end
+  end
+
+  def insert_after(val)
+    self.next_item = CircularLinkedListItem.new(val, next_item)
   end
 end
---
+++ spec/linked_lists/circular_linked_list_spec.rb
@@ -0,0 +1,71 @@
+require 'rspec'
+require 'linked_lists/circular_linked_lists.rb'
+
+describe CircularLinkedList do
+  context "sanity check" do
+    it "creates a list" do
+      CircularLinkedList.new
+    end
+
+    it "adds a head element to the list" do
+      list = CircularLinkedList.new
+      list.insert_after(nil, 1)
+      list.head.should be_a(CircularLinkedListItem)
+      list.head.value.should == 1
+    end
+
+    it "successfully replaces the head" do
+      list = CircularLinkedList.new
+      list.insert_after(nil, 1)
+      list.insert_after(nil, 2)
+
+      list.head.value.should == 2
+    end
+
+    it "finds elements in the list" do
+      list = CircularLinkedList.new
+      list.insert_after(nil, 1)
+      list.find(1).value.should == 1
+    end
+
+    it "adds more elements to the list" do
+      list = CircularLinkedList.new
+      list.insert_after(nil, 1)
+      list.insert_after(1, 2)
+      list.insert_after(1, 3)
+      list.insert_after(2, 4)
+      list.find(4).value.should == 4
+    end
+
+    it "deletes elements in the list" do
+      list = CircularLinkedList.new
+      list.insert_after(nil, 1)
+      list.insert_after(1, 2)
+      list.insert_after(1, 3)
+      list.insert_after(2, 4)
+      list.delete(4)
+
+      list.find(4).should be_nil
+    end
+
+    it "successfully removes the head from the list" do
+      list = CircularLinkedList.new
+      list.insert_after(nil, 1)
+      list.insert_after(1, 2)
+      list.insert_after(1, 3)
+      list.insert_after(2, 4)
+      list.delete(1)
+
+      list.head.value.should == 3
+    end
+
+    #Tricky for circular lists
+    it "successfully removes all the elements from the list" do
+      list = CircularLinkedList.new
+      list.insert_after(nil, 1)
+      list.delete(1)
+
+      list.head.should be_nil
+    end
+  end
+end
